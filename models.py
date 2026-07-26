550415808e161805993e6157b951b10d4f92f82c
ihomba92 <ihombakibandidavid@gmail.com>
2026-07-26 16:54:47 +0300

    added modedl to difine the tables

--- models.py
+++ models.py
@@ -1,5 +1,30 @@
-from flask_sqlalchemy import SQLAlchemy
+from app import db
 from sqlalchemy.orm import validates
-db = SQLAlchemy()
 
-# Define Models here
+class Exercise(db.Model):
+    __tablename__ = 'exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False, unique=True)
+    category = db.Column(db.String(50), nullable=False)
+    equipment_needed = db.Column(db.Boolean, default=False, nullable=False)
+
+
+class Workout(db.Model):
+    __tablename__ = 'workouts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=True)
+    notes = db.Column(db.Text, nullable=True)
+
+
+class WorkoutExercise(db.Model):
+    __tablename__ = 'workout_exercises'
+
+    id = db.Column(db.Integer, primary_key=True)
+    workout_id = db.Column(db.Integer, db.ForeignKey('workouts.id'), nullable=False)
+    exercise_id = db.Column(db.Integer, db.ForeignKey('exercises.id'), nullable=False)
+    reps = db.Column(db.Integer, nullable=True)
+    sets = db.Column(db.Integer, nullable=True)
+    duration_seconds = db.Column(db.Integer, nullable=True)
